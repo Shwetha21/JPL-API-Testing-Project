ff2fdc5a7ba6ed16e1a5f84786bde6a94c037583
Xenvastro <masimba36@outlook.com>
2020-07-13 19:25:24 +0100

    13/07/2020 - Fireball API - Implemented Service - Added base tests - Added fireball model

---
+++ JPL API Testing/FireballAPI/FireballAPIService/FBAPIService.cs
@@ -0,0 +1,23 @@
+﻿using JPL_API_Testing.FireballAPI.FireballAPIService.DataHandling;
+using JPL_API_Testing.FireballAPI.FireballAPIService.HTTPManager;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JPL_API_Testing.FireballAPI.FireballAPIService
+{
+    public class FBAPIService
+    {
+        public FireballAPIManager fireballAPIManager = new FireballAPIManager();
+        public FireballDTO fireballDTO = new FireballDTO();
+        public string fireballResponse;
+        public JObject json_fireball;
+
+        public FBAPIService()
+        {
+            fireballResponse = fireballAPIManager.GetFireball();
+            fireballDTO.DeserialiseResponse(fireballResponse);
+
+            json_fireball = JsonConvert.DeserializeObject<JObject>(fireballResponse);
+        }
+    }
+}
---
+++ JPL API Testing/FireballAPI/Tests/FireballAPITests.cs
@@ -0,0 +1,29 @@
+﻿using JPL_API_Testing.FireballAPI.FireballAPIService;
+using NUnit.Framework;
+using System.Linq;
+
+namespace JPL_API_Testing.FireballAPI.Tests
+{
+    public class FireballAPITests
+    {
+        FBAPIService _fBAPIService = new FBAPIService();
+
+        [Test]
+        public void CallingAPIReturns_CorrectSource()
+        {
+            Assert.That(_fBAPIService.fireballDTO.FireballModel.signature.source, Is.EqualTo(@"NASA/JPL Fireball Data API"));
+        }
+
+        [Test]
+        public void CallingAPIReturns_CorrectVersion()
+        {
+            Assert.That(_fBAPIService.fireballDTO.FireballModel.signature.version, Is.EqualTo("1.0"));
+        }
+
+        [Test]
+        public void CallingAPIReturns_FilledData()
+        {
+            Assert.That(_fBAPIService.fireballDTO.FireballModel.data.Count(), Is.GreaterThan(0));
+        }
+    }
+}
