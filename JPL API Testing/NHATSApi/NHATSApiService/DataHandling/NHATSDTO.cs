9188ff9c7f86e65a53835cb01a68755389a30ab6
Shwetha21 <shwetha21ashwath@gmail.com>
2020-07-15 00:38:29 +0100

    Added to test specific asteroid

--- JPL API Testing/NHATSApi/NHATSApiService/DataHandling/NHATSDTO.cs
+++ JPL API Testing/NHATSApi/NHATSApiService/DataHandling/NHATSDTO.cs
@@ -8,9 +8,16 @@ namespace JPL_API_Testing.NHATSApi.NHATSApiService.DataHandling
     {
         public NHATSRoot NHATSData { get; set; }
 
-        public void DeserializeNHATSData(string CADResponse)
+        public NHATSSpecificRoot NHATSSpecificData { get; set; }
+
+        public void DeserializeNHATSData(string NHATSResponse)
         {
-            NHATSData = JsonConvert.DeserializeObject<NHATSRoot>(CADResponse);
+            NHATSData = JsonConvert.DeserializeObject<NHATSRoot>(NHATSResponse);
+        }
+
+        public void DesirealiseNHATSSpecificData(string NHATSSpecificResponse)
+        {
+            NHATSSpecificData = JsonConvert.DeserializeObject<NHATSSpecificRoot>(NHATSSpecificResponse);
         }
     }
 }
---
+++ JPL API Testing/NHATSApi/NHATSTestsFolder/NHATSSpecificAsteroidTests.cs
@@ -0,0 +1,18 @@
+﻿using JPL_API_Testing.JPLAPIService;
+using JPL_API_Testing.NHATSApi.NHATSApiService;
+using NUnit.Framework;
+
+namespace JPL_API_Testing.NHATSApi.NHATSTestsFolder
+{
+    public class NHATSSpecificAsteroidTests
+    {
+        private NHATSAPIServices nHATSAPIServices = new NHATSAPIServices("2000%20SG344");
+
+        [Test]
+        public void CheckingWhetherTheDataObtainedIsOf2000SG344Asteroid()
+        {
+            Assert.That(nHATSAPIServices.nhatsDTO.NHATSSpecificData.fullname, Is.EqualTo("2000 SG344"));
+        }
+
+    }
+}
