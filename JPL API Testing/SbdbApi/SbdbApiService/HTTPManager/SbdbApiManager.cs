a603d345beee70d6e718a647bbef82c03c6cf896
William Millington <WMillington@SpartaGlobal.com>
2020-07-15 16:19:50 +0100

    Complete coverage of SBDB Happy path

--- JPL API Testing/SbdbApi/SbdbApiService/HTTPManager/SbdbApiManager.cs
+++ JPL API Testing/SbdbApi/SbdbApiService/HTTPManager/SbdbApiManager.cs
@@ -10,9 +10,9 @@ namespace JPL_API_Testing.SbdbApiService.HTTPManager
         {
             client = new RestClient(ConfigReader.BaseUrl);
         }
-        public string GetSmallBodyData()
+        public string GetSmallBodyData(string asteroid)
         {
-            var request = new RestRequest("sbdb.api?sstr=2015ab");
+            var request = new RestRequest("sbdb.api?sstr=" + asteroid);
             var response = client.Execute(request, Method.GET);
             return response.Content;
         }
