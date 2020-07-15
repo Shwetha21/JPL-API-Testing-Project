026dce1df60c942bfd1343a5248ab88746b68b47
Shwetha21 <shwetha21ashwath@gmail.com>
2020-07-15 11:41:58 +0100

    More tests for Orpheus asteroid

--- JPL API Testing/NHATSApi/NHATSTestsFolder/NHATSSpecificAsteroidTests.cs
+++ JPL API Testing/NHATSApi/NHATSTestsFolder/NHATSSpecificAsteroidTests.cs
@@ -7,6 +7,7 @@ namespace JPL_API_Testing.NHATSApi.NHATSTestsFolder
     public class NHATSSpecificAsteroidTests
     {
         private NHATSAPIServices nHATSAPIServices = new NHATSAPIServices("2000%20SG344");
+        private NHATSAPIServices _nhatsServiceforOrpheus = new NHATSAPIServices("3361");
 
         [Test]
         public void CheckingWhetherTheDataObtainedIsOf2000SG344Asteroid()
@@ -33,9 +34,9 @@ namespace JPL_API_Testing.NHATSApi.NHATSTestsFolder
         }
 
         [Test]
-        public void CheckingWhetherHvalueIsGreatetThan20ToConsiderNEAASNHATS()
+        public void CheckingWhetherHvalueIsGreatetThan15ToConsiderNEAASNHATS()
         {
-            Assert.That(nHATSAPIServices.nhatsDTO.NHATSSpecificData.h, Is.GreaterThan("20"));
+            Assert.That(nHATSAPIServices.nhatsDTO.NHATSSpecificData.h, Is.GreaterThan("15"));
         }
 
         [Test]
@@ -49,7 +50,49 @@ namespace JPL_API_Testing.NHATSApi.NHATSTestsFolder
         {
             Assert.That(nHATSAPIServices.nhatsDTO.NHATSSpecificData.min_dur_traj.launch, Is.EqualTo("2028-04-08"));
         }
-    
+
+        [Test]
+        public void CheckingWhetherTheDataObtainedIsOfOrpheus()
+        {
+            Assert.That(_nhatsServiceforOrpheus.nhatsDTO.NHATSSpecificData.fullname.Trim(), Is.EqualTo("3361 Orpheus (1982 HR)"));
+        }
+
+        [Test]
+        public void CheckingWhetherItsEstimatedMinimumSizeIs272()
+        {
+            Assert.That(_nhatsServiceforOrpheus.nhatsDTO.NHATSSpecificData.min_size, Is.EqualTo("272"));
+        }
+
+        [Test]
+        public void CheckingWhetherEstimatedMaximumSizeIs1216()
+        {
+            Assert.That(_nhatsServiceforOrpheus.nhatsDTO.NHATSSpecificData.max_size, Is.EqualTo("1216"));
+        }
+
+        [Test]
+        public void CheckingWhetherTotalDurationOfTheMissionIs362()
+        {
+            Assert.That(_nhatsServiceforOrpheus.nhatsDTO.NHATSSpecificData.min_dv_traj.dur_total, Is.EqualTo("362"));
+        }
+
+        [Test]
+        public void CheckingWhetherHvalueofOrpheusIsGreatetThan15ToConsiderNEAASNHATS()
+        {
+            Assert.That(_nhatsServiceforOrpheus.nhatsDTO.NHATSSpecificData.h, Is.GreaterThan("15"));
+        }
+
+        [Test]
+        public void ChechingForTheValueOfOCCIs0()
+        {
+            Assert.That(_nhatsServiceforOrpheus.nhatsDTO.NHATSSpecificData.occ, Is.EqualTo("0"));
+        }
+
+        [Test]
+        public void CheckingForThelaunchDateOfOrpheus()
+        {
+            Assert.That(_nhatsServiceforOrpheus.nhatsDTO.NHATSSpecificData.min_dur_traj.launch, Is.EqualTo("2021-11-15"));
+        }
+
 
     }
 }
