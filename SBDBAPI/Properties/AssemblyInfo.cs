49298db7332d9cd05c2796ba12627b7b23764376
William Millington <WMillington@SpartaGlobal.com>
2020-07-16 12:54:26 +0100

    Separate SBDB into a new project

---
+++ SBDBAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SBDBAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OVH")]
+[assembly: AssemblyProduct("SBDBAPI")]
+[assembly: AssemblyCopyright("Copyright © OVH 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("55e7ba0f-5d7e-4858-89d9-36a9e746604e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
