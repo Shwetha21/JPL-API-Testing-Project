63cdce1e7d68e50d6d19dd67f269f13b4b0e40fd
Shwetha21 <shwetha21ashwath@gmail.com>
2020-07-16 14:08:27 +0100

    Scouts project Added

---
+++ SCOUTS API/ScoutDataApi/ScoutDataApiService/DataHandling/ScoutDTO.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace JPL_API_Testing.ScoutDataApi.ScoutDataApiService.DataHandling
+{
+    public class ScoutDTO
+    {
+        public ScoutRoot ScoutData { get; set; }
+
+        public void DeserializeScoutData(string ScoutResponse)
+        {
+            ScoutData = JsonConvert.DeserializeObject<ScoutRoot>(ScoutResponse);
+        }
+    }
+}
