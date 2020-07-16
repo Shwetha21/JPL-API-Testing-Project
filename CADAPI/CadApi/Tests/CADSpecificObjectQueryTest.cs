c47e5bbbd4d10ed884bbcdd9a09e191ffb76cf78
William Millington <WMillington@SpartaGlobal.com>
2020-07-16 17:38:06 +0100

    Adding Query Tests on CAD

---
+++ CADAPI/CadApi/Tests/CADSpecificObjectQueryTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JPL_API_Testing.JPLAPIService;
+using NUnit.Framework;
+
+namespace CADAPI.CadApi.Tests
+{
+    class CADSpecificObjectQueryTest
+    {
+        private APIService _cadService = new APIService(3745588);
+        
+        [Test]
+        public void CallingUniqueSPK_ReturnsOneResult()
+        {
+            Assert.That(_cadService.cadDataList.Count(), Is.EqualTo(1));
+        }
+    }
+}
